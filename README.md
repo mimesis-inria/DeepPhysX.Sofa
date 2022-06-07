331760b885aee791d29f0610235765987f2dc7e0
RobinEnjalbert <robin.enjalbrt@gmail.com>
2022-06-07 12:22:39 +0200

    Fix install process

--- README.md
+++ README.md
@@ -1,24 +1,17 @@
-# DeepPhysX_Sofa
+# DeepPhysX.Sofa
 
 This python package is part of the [DeepPhysX](https://github.com/mimesis-inria/DeepPhysX) project.
 It contains adaptations of some Core components that are compatible with the [SOFA](https://www.sofa-framework.org/) 
 framework.
 
-### Installation
+### Quick install
 
-Cloning the main package is of course mandatory:
+The package requires [DeepPhysX](https://github.com/mimesis-inria/DeepPhysX) to be installed.
+Furthermore, it will require [SOFA bindings](https://sofapython3.readthedocs.io/en/latest/) to be used.
 
-```bash
-$ git clone https://github.com/mimesis-inria/DeepPhysX.git
-```
-
-This repository is automatically cloned when configuring the [DeepPhysX](https://github.com/mimesis-inria/DeepPhysX) 
-installation.
-Otherwise, it can also be cloned manually in a `Sofa` directory:
+The easiest way to install is using `pip`, but there are a several way to install and configure a **DeepPhysX**
+environment (refer to the [**documentation**](https://deepphysx.readthedocs.io) for further instructions).
 
 ```bash
-$ cd DeepPhysX
-$ git clone https://github.com/mimesis-inria/DeepPhysX_Sofa.git Sofa
+$ pip install DeepPhysX.Sofa
 ```
-
-This package will be automatically detected when configuring the project installation.
--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 from setuptools import setup, find_packages
-from os import chdir, sep, rename
-from os.path import abspath, join, pardir
+from os.path import join
 
 PROJECT = 'DeepPhysX'
 PACKAGE = 'Sofa'
 DEPENDENCIES = ['DeepPhysX']
-
-# Check current repository
-repository = abspath(join(__file__, pardir)).split(sep)[-1]
-if repository != PACKAGE:
-    print(f"WARNING: Wrong current repository name, renaming '{repository}' --> '{PACKAGE}'")
-    chdir(pardir)
-    rename(repository, PACKAGE)
-    chdir(PACKAGE)
 
 # Configure packages and subpackages list and dependencies list
 packages = [f'{PROJECT}.{PACKAGE}']
@@ -21,10 +12,16 @@ for sub_package in find_packages(where='src'):
     packages.append(f'{PROJECT}.{PACKAGE}.{sub_package}')
     packages_dir[f'{PROJECT}.{PACKAGE}.{sub_package}'] = join('src', *sub_package.split('.'))
 
+# Extract README.md content
+with open('README.md') as f:
+    long_description = f.read()
+
 # Installation
-setup(name=f'{PROJECT}_{PACKAGE}',
-      version='1.0',
-      description='A Python framework interfacing AI with numerical simulation.',
+setup(name=f'{PROJECT}.{PACKAGE}',
+      version='22.06',
+      description='A SOFA compatible package for DeepPhysX framework.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Mimesis',
       author_email='robin.enjalbert@inria.fr',
       url='https://github.com/mimesis-inria/DeepPhysX_Sofa',
