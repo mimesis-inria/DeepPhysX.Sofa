d3fb854332eb28fe630fadfdb60b9934f275bb2f
robin <robin.enjalbert@inria.fr>
2022-11-24 14:19:58 +0100

    Include examples in installation.

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from os.path import join
 
 PROJECT = 'DeepPhysX'
 PACKAGE = 'Sofa'
-DEPENDENCIES = ['DeepPhysX']
 
-# Configure packages and subpackages list and dependencies list
 packages = [f'{PROJECT}.{PACKAGE}']
 packages_dir = {f'{PROJECT}.{PACKAGE}': 'src'}
+
+# Configure packages list and directories
 for sub_package in find_packages(where='src'):
     packages.append(f'{PROJECT}.{PACKAGE}.{sub_package}')
     packages_dir[f'{PROJECT}.{PACKAGE}.{sub_package}'] = join('src', *sub_package.split('.'))
+
+# Add examples as subpackages
+packages.append(f'{PROJECT}.examples.{PACKAGE}')
+packages_dir[f'{PROJECT}.examples.{PACKAGE}'] = 'examples'
+for example_dir in find_packages(where='examples'):
+    packages.append(f'{PROJECT}.examples.{PACKAGE}.{example_dir}')
 
 # Extract README.md content
 with open('README.md') as f:
@@ -18,13 +24,13 @@ with open('README.md') as f:
 
 # Installation
 setup(name=f'{PROJECT}.{PACKAGE}',
-      version='22.06',
-      description='A SOFA compatible package for DeepPhysX framework.',
+      version='22.12',
+      description='A SOFA compatibility layer for DeepPhysX framework.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Mimesis',
       author_email='robin.enjalbert@inria.fr',
-      url='https://github.com/mimesis-inria/DeepPhysX_Sofa',
+      url='https://github.com/mimesis-inria/DeepPhysX.Sofa',
       packages=packages,
       package_dir=packages_dir,
-      install_requires=DEPENDENCIES)
+      install_requires=['DeepPhysX'])
