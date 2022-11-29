5dca7ef42f9470cb2943c89105cbc31db8f7c0e9
robin <robin.enjalbert@inria.fr>
2022-11-29 17:53:42 +0100

    Update the documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ from os.path import abspath, join, pardir
 import sys
 
 # DeepPhysX root
-root = abspath(join(abspath(__file__), pardir, pardir, pardir, 'src'))
+root = abspath(join(abspath(__file__), pardir, pardir, pardir, 'src', 'Sofa'))
 all_modules = ['Environment', 'Pipeline']
 
 # Import all modules
